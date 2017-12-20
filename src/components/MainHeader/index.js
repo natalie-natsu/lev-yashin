86cf52bf4b13f32990ec1d46278464cc8532739f
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2017-12-20 02:07:00 +0100

    SignIn modal replacement

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -6,69 +6,32 @@ import { action as toggleMenu } from 'redux-burger-menu';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/fontawesome-free-solid';
-import { faUserCircle } from '@fortawesome/fontawesome-free-regular';
 
 import './MainHeader.scss';
-import SignInForm from '../Authentication/SignInForm';
 import Languages from '../Languages';
 
-class MainHeader extends React.Component {
-    render() {
-        const { burgerMenu, dispatch, t } = this.props;
-
-        return (
-            <header id="main-header">
-                <div
-                    className="modal fade"
-                    id="modal-mainHeader-login"
-                    tabIndex="-1"
-                    role="dialog"
-                    aria-labelledby={t('form:signIn.labelledBy')}
+const MainHeader = ({ burgerMenu, dispatch, t }) => (
+    <header id="main-header">
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <a href="/" className="navbar-brand mb-0 h1 hidden-sm-down">{t('project.name')}</a>
+            <div className="btn-drawer my-2 ml-2 ml-sm-0 mr-2 mr-sm-3">
+                <button
+                    className="btn"
+                    type="button"
+                    aria-expanded="false"
+                    aria-label={t('accessibility.aria-label.toggleNav')}
+                    onClick={() => dispatch(toggleMenu(!burgerMenu.isOpen))}
+                    onKeyPress={() => dispatch(toggleMenu(!burgerMenu.isOpen))}
                 >
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">
-                                    <FontAwesomeIcon icon={faUserCircle} />&nbsp;
-                                    {t('component:MainHeader.modal-login.title')}
-                                </h5>
-                                <button
-                                    type="button"
-                                    className="close"
-                                    data-dismiss="modal"
-                                    aria-label={t('accessibility.aria-label.close')}
-                                > <span >&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <SignInForm onSubmit={() => this.signIn()} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <nav className="navbar navbar-expand-lg navbar-dark">
-                    <a href="/" className="navbar-brand mb-0 h1 hidden-sm-down">{t('project.name')}</a>
-                    <div className="btn-drawer my-2 ml-2 ml-sm-0 mr-2 mr-sm-3">
-                        <button
-                            className="btn"
-                            type="button"
-                            aria-expanded="false"
-                            aria-label={t('accessibility.aria-label.toggleNav')}
-                            onClick={() => dispatch(toggleMenu(!burgerMenu.isOpen))}
-                            onKeyPress={() => dispatch(toggleMenu(!burgerMenu.isOpen))}
-                        >
-                            <FontAwesomeIcon icon={faBars} />
-                        </button>
-                    </div>
-                    <span className="hidden-sm-down mr-3"><Languages /></span>
-                    <p id="main-header-title-dom" className="navbar-text mr-auto" />
-                    <div id="main-header-side-action-dom" />
-                </nav>
-            </header>
-        );
-    }
-}
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+            </div>
+            <span className="hidden-sm-down mr-3"><Languages /></span>
+            <p id="main-header-title-dom" className="navbar-text mr-auto" />
+            <div id="main-header-side-action-dom" />
+        </nav>
+    </header>
+);
 
 MainHeader.propTypes = {
     burgerMenu: PropTypes.shape({
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import { SubmissionError } from 'redux-form';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faSpinner, faSignInAlt } from '@fortawesome/fontawesome-free-solid';
+import { faUserCircle } from '@fortawesome/fontawesome-free-regular';
 
 import { failSignIn, fetchProfileIfNeeded, requestSignIn, signOut, successSignIn } from '../../actions/authentication';
 import { getEndpoint, headers } from '../../helpers/endpoint';
@@ -14,6 +15,7 @@ import Landing from './Landing';
 import Description from './Description';
 import SideAction from '../../components/MainHeader/SideAction';
 import Title from '../../components/MainHeader/Title';
+import SignInForm from '../../components/Authentication/SignInForm';
 
 const { $ } = window;
 
@@ -73,10 +75,39 @@ class Home extends React.Component {
     }
 
     render() {
+        const { t } = this.props;
         return (
             <div id="home">
                 <Title>Hey ! Bienvenue sur la HomePage ma gueule. :-)</Title>
                 <SideAction><div className="mx-2 mx-sm-3">{this.renderSignInButton()}</div></SideAction>
+                <div
+                    className="modal fade"
+                    id="modal-mainHeader-login"
+                    tabIndex="-1"
+                    role="dialog"
+                    aria-labelledby={t('form:signIn.labelledBy')}
+                >
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">
+                                    <FontAwesomeIcon icon={faUserCircle} />&nbsp;
+                                    {t('component:MainHeader.modal-login.title')}
+                                </h5>
+                                <button
+                                    type="button"
+                                    className="close"
+                                    data-dismiss="modal"
+                                    aria-label={t('accessibility.aria-label.close')}
+                                > <span >&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <SignInForm onSubmit={() => this.signIn()} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Landing />
                 <Description />
             </div>
