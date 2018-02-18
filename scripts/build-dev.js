1fdde32054ef17f4b75f3166108699b10af41f20
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2018-02-18 18:34:12 +0100

    API configuration

--- scripts/build-dev.js
+++ scripts/build-dev.js
@@ -3,8 +3,8 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
-process.env.API_PATH = 'http://37.59.127.200:8080';
-process.env.WS_PATH = 'ws://37.59.127.200:8080';
+process.env.API_PATH = 'http://api.worldcup.game';
+process.env.WS_PATH = 'ws://api.worldcup.game';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
